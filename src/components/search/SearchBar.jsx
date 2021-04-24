ddb8f5bf7c229895b147e731fd862d87cbf0c381
woojoo <wown387@naver.com>
2021-04-24 17:24:42 +0900

    QA search error

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,46 +1,42 @@
-
-import useAutocomplete from '@material-ui/lab/useAutocomplete';
-import top100Films from './SearchData'
-import SearchIcon from '@material-ui/icons/Search';
-import { makeStyles } from '@material-ui/core/styles';
+import useAutocomplete from "@material-ui/lab/useAutocomplete";
+import top100Films from "./SearchData";
+import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles } from "@material-ui/core/styles";
 import React, { useState } from "react";
 const useStyles = makeStyles((theme) => ({
-  SearchIcon:{
-    marginTop:8,
-    position: 'absolute',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+  SearchIcon: {
+    marginTop: 8,
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    marginLeft: 30,
+    width: "90%",
+  },
 
-  },
-  input:{
-    marginLeft:30,
-    width:"90%",
-  },
-  
   listbox: {
-    fontSize:20,
-    width:"90%",
-    marginTop:15 ,
+    fontSize: 20,
+    width: "90%",
+    marginTop: 15,
     padding: 10,
     zIndex: 1,
-    position: 'absolute',
-    listStyle: 'none',
+    position: "absolute",
+    listStyle: "none",
     backgroundColor: theme.palette.background.paper,
-    overflow: 'auto',
+    overflow: "auto",
     maxHeight: 400,
     borderRadius: "7px",
-    border: '1px solid rgba(0,0,0,.25)',
+    border: "1px solid rgba(0,0,0,.25)",
     '& li[data-focus="true"]': {
-      backgroundColor: '#4a8df6',
-      color: 'white',
-      cursor: 'pointer',
-    
+      backgroundColor: "#4a8df6",
+      color: "white",
+      cursor: "pointer",
     },
-    '& li:active': {
-      backgroundColor: '#2977f5',
-      color: 'white',
-     
+    "& li:active": {
+      backgroundColor: "#2977f5",
+      color: "white",
     },
   },
 }));
@@ -48,19 +44,20 @@ const useStyles = makeStyles((theme) => ({
 export default function UseAutocomplete(props) {
   const classes = useStyles();
 
-  const [key,setKey]=useState("");
- 
-  function keyPress(event){
+  const [key, setKey] = useState("");
 
-      if(event.keyCode === 13 && event.target.value ){
-        console.log()
-        event.preventDefault()
-        props.search(event.target.value)
-     }
-     else{
-       setKey(event.target.value)
-
-     }
+  function keyPress(event) {
+    if (
+      event.keyCode === 13 &&
+      event.target.value &&
+      groupedOptions.length == 0
+    ) {
+      console.log();
+      event.preventDefault();
+      props.search(event.target.value);
+    } else {
+      setKey(event.target.value);
+    }
   }
   const {
     getRootProps,
@@ -69,34 +66,39 @@ export default function UseAutocomplete(props) {
     getOptionProps,
     groupedOptions,
   } = useAutocomplete({
-    id: 'use-autocomplete-demo',
+    id: "use-autocomplete-demo",
     options: top100Films,
-    getOptionLabel: (option) => { 
-       return option.title}
+    getOptionLabel: (option) => {
+      return option.title;
+    },
   });
 
   return (
-  <div>
-  <form>
-      <div  {...getRootProps()}>
-        <SearchIcon className={classes.SearchIcon} />
-        <input className={classes.input}
-               onKeyDown={keyPress}
-               placeholder="search city name"
-              
-         {...getInputProps()} />
-
-      </div>
-      {groupedOptions.length > 0 ? (
-        <ul   className={classes.listbox} {...getListboxProps()}>
-          {groupedOptions.filter(option=>key.toLowerCase()=== option.title.slice(1,key.length+1).toLowerCase()).map((option, index) => ( <li  {...getOptionProps({ option, index })}> {option.title}  </li>))
-          }
-          
-        </ul>
-      ) : null}
-       
-       </form>
+    <div>
+      <form>
+        <div {...getRootProps()}>
+          <SearchIcon className={classes.SearchIcon} />
+          <input
+            className={classes.input}
+            onKeyDown={keyPress}
+            placeholder="search city name"
+            {...getInputProps()}
+          />
+        </div>
+        {groupedOptions.length > 0 ? (
+          <ul className={classes.listbox} {...getListboxProps()}>
+            {groupedOptions
+              .filter(
+                (option) =>
+                  key.toLowerCase() ===
+                  option.title.slice(1, key.length + 1).toLowerCase()
+              )
+              .map((option, index) => (
+                <li {...getOptionProps({ option, index })}> {option.title} </li>
+              ))}
+          </ul>
+        ) : null}
+      </form>
     </div>
- 
   );
 }
